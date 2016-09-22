f8f23921a677cabcaa53fb80010b91c8660cbd95
ZZPot <you@example.com>
2016-09-22 19:26:21 +1200

    add log10 and pow10

--- main.cpp
+++ main.cpp
@@ -20,12 +20,15 @@ Mat LMS_to_lab_1 = (Mat_<double>(3,3) << 1/sqrt(3), 0, 0,
 Mat LMS_to_lab_2 = (Mat_<double>(3,3) <<	1, 1, 1,
 											1, 1, -2,
 											1, -1, 0);
+Mat LMS_to_lab, lab_to_LMS;
+
 struct ct_image
 {
-	std::string first;
-	std::string second;
-	std::string morph;
+	std::string source;
+	std::string target;
+	std::string result;
 };
+
 bool makeCT(ct_image images);
 Mat convertTolab(Mat input);
 Mat convertFromlab(Mat input);
@@ -34,12 +37,16 @@ int main()
 {
 	//transpose(RGB_to_LMS, RGB_to_LMS);
 	//transpose(LMS_to_RGB, LMS_to_RGB);
-	transpose(LMS_to_lab_1, LMS_to_lab_1);
-	transpose(LMS_to_lab_2, LMS_to_lab_2);
+	//GEMM_1_T GEMM_2_T GEMM_3_T
+	gemm(LMS_to_lab_1, LMS_to_lab_2, 1, Mat(), 0, LMS_to_lab);
+	gemm(LMS_to_lab_2, LMS_to_lab_1, 1, Mat(), 0, lab_to_LMS, GEMM_1_T);
+	//transpose(LMS_to_lab_1, LMS_to_lab_1);
+	//transpose(LMS_to_lab_2, LMS_to_lab_2);
+
 	ct_image images_1 = {"pic_1.jpg", "pic_2.jpg", "pic_1_2(1).jpg"};
 	if(makeCT(images_1))
 	{
-		Mat res_pic = imread(images_1.morph);
+		Mat res_pic = imread(images_1.result);
 		imshow(WND_NAME_RES, res_pic);
 		waitKey(0);
 	}
@@ -48,32 +55,29 @@ int main()
 
 bool makeCT(ct_image images)
 {
-	Mat img1 = imread(images.first);
-	Mat img2 = imread(images.second);
+	Mat imgs = imread(images.source);
+	Mat imgt = imread(images.target);
 
-	Mat img1_lab = convertTolab(img1);
-	Mat img2_lab = convertTolab(img2);
+	Mat imgs_lab = convertTolab(imgs);
+	Mat imgt_lab = convertTolab(imgt);
 	
 
-	Mat mean1, mean2, stdd1, stdd2;
-	meanStdDev(img1_lab, mean1, stdd1);
-	meanStdDev(img2_lab, mean2, stdd2);
+	Mat meant, means, stddt, stdds;
+	meanStdDev(imgs_lab, means, stdds);
+	meanStdDev(imgt_lab, meant, stddt);
 
-	std::vector<Mat> img1_lab_channels, img2_lab_channels;
-	split(img1_lab, img1_lab_channels);
-	split(img2_lab, img2_lab_channels);
-
+	std::vector<Mat> imgs_lab_channels, imgt_lab_channels;
+	split(imgs_lab, imgs_lab_channels);
+	split(imgt_lab, imgt_lab_channels);
 	for(int i = 0; i < 3; i++) 
 	{
-		img1_lab_channels[i] -= mean1.at<double>(i);
-		img1_lab_channels[i] *= (stdd1.at<double>(i) / stdd2.
-		at<double>(i));
-		img1_lab_channels[i] += mean1.at<double>(i);
+		imgs_lab_channels[i] = (imgs_lab_channels[i] - means.at<double>(i)) * 
+								stddt.at<double>(i)/stdds.at<double>(i) + meant.at<double>(i);
 	}
 	Mat result;
-	merge(img1_lab_channels, result);
+	merge(imgs_lab_channels, result);
 	result = convertFromlab(result);
-	imwrite(images.morph, result);
+	imwrite(images.result, result);
 	return true;
 }
 
@@ -83,21 +87,28 @@ Mat convertTolab(Mat input)
 	input.convertTo(img,CV_32FC1);
 	Mat img_lms;
 	transform(img, img_lms, RGB_to_LMS);
-	// log10 !!!!!!!!
+	MatIterator_<Vec3f> iter = img_lms.begin<Vec3f>();
+	for(; iter != img_lms.end<Vec3f>(); iter++)
+	{
+		(*iter)[0] = log10((*iter)[0]);
+		(*iter)[1] = log10((*iter)[1]);
+		(*iter)[2] = log10((*iter)[2]);
+	}
 	Mat img_lab;
-	transform(img_lms, img_lab, LMS_to_lab_2);
-	transform(img_lab, img_lab, LMS_to_lab_1);
+	transform(img_lms, img_lab, LMS_to_lab);
 	return img_lab;
 }
 Mat convertFromlab(Mat input)
 {
 	Mat img_lms;
-	Mat trans1, trans2;
-	transpose(LMS_to_lab_1, trans1);
-	transpose(LMS_to_lab_2, trans2);
-	transform(input, img_lms, trans1);
-	transform(img_lms, img_lms, trans2);
-	// pow10 !!!!!!!!!
+	transform(input, img_lms, lab_to_LMS);
+	MatIterator_<Vec3f> iter = img_lms.begin<Vec3f>();
+	for(; iter != img_lms.end<Vec3f>(); iter++)
+	{
+		(*iter)[0] = pow((*iter)[0], 10);
+		(*iter)[1] = pow((*iter)[1], 10);
+		(*iter)[2] = pow((*iter)[2], 10);
+	}
 	Mat img_RGB;
 	transform(img_lms, img_RGB, LMS_to_RGB);
 	img_RGB.convertTo(img_RGB,CV_8UC1);
