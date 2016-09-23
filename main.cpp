7266d800cff6ce481fb0dc6f62787686c268b5fe
ZZPot <you@example.com>
2016-09-23 13:39:05 +1200

    more images

--- main.cpp
+++ main.cpp
@@ -8,19 +8,28 @@ using namespace cv;
 
 #define WND_NAME_RES "Result Picture"
 
-Mat RGB_to_LMS = (Mat_<double>(3,3) <<	0.3811, 0.5783, 0.0402,
+Mat RGB_to_LMS = (Mat_<float>(3,3) <<	0.3811, 0.5783, 0.0402,
 										0.1967, 0.7244, 0.0782,
 										0.0241, 0.1288, 0.8444);
-Mat LMS_to_RGB = (Mat_<double>(3,3) <<	4.4679, -3.5873, 0.1193,
+Mat LMS_to_RGB = (Mat_<float>(3,3) <<	4.4679, -3.5873, 0.1193,
 										-1.2186, 2.3809, -0.1624,
 										0.0497, -0.2439, 1.2045);
-Mat LMS_to_lab_1 = (Mat_<double>(3,3) << 1/sqrt(3), 0, 0,
+Mat LMS_to_lab_1 = (Mat_<float>(3,3) << 1/sqrt(3), 0, 0,
 										0, 1/sqrt(6), 0,
 										0, 0, 1/sqrt(2));
-Mat LMS_to_lab_2 = (Mat_<double>(3,3) << 1, 1, 1,
+Mat LMS_to_lab_2 = (Mat_<float>(3,3) << 1, 1, 1,
 											1, 1, -2,
 											1, -1, 0);
-Mat LMS_to_lab, lab_to_LMS;
+float _x = 1/sqrt(3), _y = 1/sqrt(6), _z = 1/sqrt(2);
+
+Mat LMS_to_lab = (Mat_<float>(3,3) <<	_x, _x, _x,
+										_y, _y, -2*_y,
+										_z, -_z, 0);
+
+Mat lab_to_LMS = (Mat_<float>(3,3) <<	_x, _y, _z,
+										_x, _y, -_z,
+										_x, -2*_y, 0);
+
 struct ct_image
 {
 	std::string source;
@@ -32,22 +41,23 @@ bool makeCT(ct_image images);
 
 Mat convertTolab(Mat input);
 Mat convertFromlab(Mat input);
-Mat mult(Mat mat1, Mat mat2);
 Mat _transform(Mat mat, Mat core);
 
 int main()
 {
-	//transpose(RGB_to_LMS, RGB_to_LMS);
-	//transpose(LMS_to_RGB, LMS_to_RGB);
-	//GEMM_1_T GEMM_2_T GEMM_3_T
-	//gemm(LMS_to_lab_1, LMS_to_lab_2, 1, Mat(), 0, LMS_to_lab, GEMM_2_T);
-	//gemm(LMS_to_lab_2, LMS_to_lab_1, 1, Mat(), 0, lab_to_LMS, GEMM_1_T);
-	transpose(LMS_to_lab_1, LMS_to_lab_1);
-	transpose(LMS_to_lab_2, LMS_to_lab_2);
+	transpose(RGB_to_LMS, RGB_to_LMS);
+	transpose(LMS_to_RGB, LMS_to_RGB);
 	ct_image images_1 = {"pic_1.jpg", "pic_2.jpg", "pic_1_2(1).jpg"};
+	ct_image images_2 = {"img_3.jpg", "img_4.jpg", "img_3_4(1).jpg"};
 	if(makeCT(images_1))
 	{
 		Mat res_pic = imread(images_1.result);
+		imshow(WND_NAME_RES, res_pic);
+		waitKey(0);
+	}
+	if(makeCT(images_2))
+	{
+		Mat res_pic = imread(images_2.result);
 		imshow(WND_NAME_RES, res_pic);
 		waitKey(0);
 	}
@@ -57,22 +67,36 @@ bool makeCT(ct_image images)
 {
 	Mat imgs = imread(images.source);
 	Mat imgt = imread(images.target);
-	Mat imgs_lab = convertTolab(imgs);
-	Mat imgt_lab = convertTolab(imgt);
+	Mat imgs_lab;
+	Mat imgt_lab;
+	cvtColor(imgs, imgs_lab, CV_BGR2Lab);
+	cvtColor(imgt, imgt_lab, CV_BGR2Lab);
+	imgs.convertTo(imgs, CV_64FC1);
+	imgt.convertTo(imgt, CV_64FC1);
+
 	Mat meant, means, stddt, stdds;
 	meanStdDev(imgs_lab, means, stdds);
 	meanStdDev(imgt_lab, meant, stddt);
+
 	std::vector<Mat> imgs_lab_channels, imgt_lab_channels;
 	split(imgs_lab, imgs_lab_channels);
 	split(imgt_lab, imgt_lab_channels);
+
 	for(int i = 0; i < 3; i++)
 	{
 		imgs_lab_channels[i] = (imgs_lab_channels[i] - means.at<double>(i)) *
 		stddt.at<double>(i)/stdds.at<double>(i) + meant.at<double>(i);
+
+		/*imgs_lab_channels[i] -= means.at<double>(i);
+		imgs_lab_channels[i] *= stddt.at<double>(i)/stdds.at<double>(i);
+		imgs_lab_channels[i] += meant.at<double>(i); */
 	}
+
 	Mat result;
 	merge(imgs_lab_channels, result);
-	result = convertFromlab(result);
+
+	result.convertTo(result, CV_8UC1);
+	cvtColor(result, result, CV_Lab2BGR);
 	imwrite(images.result, result);
 	return true;
 }
@@ -90,17 +114,13 @@ Mat convertTolab(Mat input)
 		(*iter)[2] = log10((*iter)[2]);
 	}
 	Mat img_lab;
-	img_lab = _transform(img_lms, LMS_to_lab_2);
-	img_lab = _transform(img_lab, LMS_to_lab_1);
+	img_lab = _transform(img_lms, LMS_to_lab);
 	return img_lab;
 }
 Mat convertFromlab(Mat input)
 {
 	Mat img_lms;
-	transpose(LMS_to_lab_1, LMS_to_lab_1);
-	transpose(LMS_to_lab_2, LMS_to_lab_2);
-	img_lms = _transform(input, LMS_to_lab_1);
-	img_lms = _transform(img_lms, LMS_to_lab_2);
+	img_lms = _transform(input, lab_to_LMS);
 	MatIterator_<Vec3f> iter = img_lms.begin<Vec3f>();
 	for(; iter != img_lms.end<Vec3f>(); iter++)
 	{
@@ -112,17 +132,6 @@ Mat convertFromlab(Mat input)
 	img_RGB = _transform(img_lms, LMS_to_RGB);
 	img_RGB.convertTo(img_RGB,CV_8UC1);
 	return img_RGB;
-}
-Mat mult(Mat mat1, Mat mat2)
-{
-	Mat res = Mat::zeros(mat1.rows, mat2.cols, CV_32FC3);
-	for(int i = 0; i < mat1.rows; i++)
-	for(int j = 0; j < mat2.cols; j++)
-	{
-		for(int k = 0; k < mat1.cols; k++)
-			res.at<float>(i, j) += mat1.at<float>(i, k) * mat2.at<float>(k, j);
-	}
-	return res;
 }
 Mat _transform(Mat mat, Mat core)
 {
